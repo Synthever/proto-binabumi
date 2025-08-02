63b20f37508d5badeb7e569c22f4cc1927693ce4
synthever <126140590+Synthever@users.noreply.github.com>
2025-08-02 22:07:16 +0700

    Import AuthController in web routes
    Added the AuthController import to routes/web.php to enable routing to authentication-related controller actions.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
