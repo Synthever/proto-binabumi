4e30e357b9bcec5c2471c21b33fc023168b64f1b
calistakiraa <calista.kira@students.amikom.ac.id>
2025-08-21 23:04:47 +0700

    back btn lagi

--- resources/views/maps/cari-mesin-1.blade.php
+++ resources/views/maps/cari-mesin-1.blade.php
@@ -6,6 +6,7 @@
     <title>Cari Mesin - SIGMA</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('/assets/css/maps/cari-mesin.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body class="bg-gray-50">
